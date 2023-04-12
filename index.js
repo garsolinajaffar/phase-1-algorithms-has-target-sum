be05e884f66c621821cebb7d0eccd197cdea5f71
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-04-12 16:22:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,23 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(arr, target) {
+  // code goes here
 }
-
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
-
-  console.log("");
-
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+function hasTargetSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
-
-module.exports = hasTargetSum;
+function hasTargetSum(arr, target) {
+  const differences = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (differences[arr[i]]) {
+      return true;
+    }
+    differences[target - arr[i]] = true;
+  }
+  return false;
+}
